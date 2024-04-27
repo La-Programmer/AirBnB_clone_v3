5f2b93b7377c800eb31717b230f99544ebb7f1cd
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-04-27 17:14:15 +0100

    FEAT: Adding endpoint to get the count of all objects in storage according to their types

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 
 from flask import jsonify
+from models import storage
+from models.engine.db_storage import classes
 from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """Retrieves the number of each object by type"""
+    stat_obj = {}
+    for key in classes:
+        stat_obj[key] = storage.count(key)
+    return jsonify(stat_obj)
