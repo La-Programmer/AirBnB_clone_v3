535beacdc2c1d265fc30a478288f54991f6f5b18
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-29 19:21:54 +0100

    could only create the city data fetching cos of lack of database

---
+++ api/v1/views/cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""flask view for the city class"""
+from models.state import City
+from flask import jsonify, abort, request
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('GET /api/v1/cities/<city_id>', methods=['GET'], strict_slashes=False)
+def get_city(city_id):
+    """Gets the city data with the city ID"""
+    city = storage.get('City', city_id)
+    new_state = []
+    new_state.append(city)
+    return new_state, 200
+        
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+def get_city(state_id):
+    city = storage.get('City', state_id)          
