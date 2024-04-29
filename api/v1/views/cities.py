21b2bd4ddb7bc6402255dd71813a83385e6ff347
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-29 20:03:23 +0100

    updated the city view to handle get request properly

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,10 +10,18 @@ from api.v1.views import app_views
 def get_city(city_id):
     """Gets the city data with the city ID"""
     city = storage.get('City', city_id)
-    new_state = []
-    new_state.append(city)
-    return new_state, 200
+    if not city:
+        abort(404)
+    return city, 200
         
 @app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
-def get_city(state_id):
-    city = storage.get('City', state_id)          
+def get_cities(state_id):
+    state = storage.get('State', state_id)
+    if not state:
+        abort(404)
+    result = []
+    city = storage.all('City')
+    for key in city:
+        if city[key].state_id == state_id:
+            result.append(city[key])
+    return jsonify(result), 200
